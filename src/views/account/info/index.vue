71ed53ef455920db8c3bd247f96d656fca08a7d7
李园园 <yuanyuan.li@aiads.com>
2018-12-28 19:32:37 +0800

    账号信息

--- src/views/account/info/index.vue
+++ src/views/account/info/index.vue
@@ -1,21 +1,148 @@
 <template>
-  <div class="page">
-    账号信息<br><br>
-    <router-link :to="{ name: 'account-info-edit' }">编辑账号信息</router-link>
+  <div class="page bgHei">
+     <com-statu :statuCode="statuCode"></com-statu>
+    <div class="content">
+      <h3>登录信息</h3>
+      <ul>
+        <li class="flex-box" v-for="item in loginMes">
+          <div>{{item.label}}</div>
+          <p>{{item.name}}</p>
+        </li>
+      </ul>
+      <h3>公司信息</h3>
+      <dl>
+        <dd v-for="item in company">
+          <div>{{item.label}}</div>
+          <p>{{item.name}}</p>
+        </dd>
+      </dl>
+      <h3>开户信息</h3>
+      <ul>
+        <li class="flex-box" v-for="item in opens.list">
+          <div>{{item.label}}</div>
+          <p>{{item.name}}</p>
+        </li>
+        <li class="flex-box">
+          <div>{{opens.imgs.label}}</div>
+          <p>
+            <img class="imgs" v-for="item in opens.imgs.list" :src="item" alt="alias" width="150px">
+          </p>
+        </li>
+      </ul>
+    </div>
+    <div class="btnCenter">
+      <button class="submitBtn">
+        <router-link tag="span" :to="{ name: 'account-info-edit' }">编辑账号信息</router-link>
+      </button>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Mixins } from 'vue-property-decorator'
+import { Component } from 'vue-property-decorator'
 import ViewBase from '@/util/ViewBase'
 import UrlManager from '@/util/UrlManager'
+import { infoIndex } from '@/api/info'
+import comStatu from './status.vue'
 
-@Component
+@Component({
+  components: {
+    comStatu
+  }
+})
 export default class Main extends UrlManager {
+  statuCode = 0
+  loginMes = [
+    { label: '账号类型', name: '公司信息/资源方' },
+    { label: '账号ID', name: '22' },
+    { label: '登录账号', name: 'xxxx.$com' }
+  ]
+
+  company = [
+    { label: '公司名称', name: '公司信息/资源方' },
+    { label: '账号ID', name: '22' },
+    { label: '登录账号', name: 'xxxx.$com' },
+    { label: '账号ID', name: '22' },
+    { label: '登录账号', name: 'xxxx.$com' }
+  ]
+
+  opens = {
+    list: [
+      { label: '资质类型', name: '公司信息/资源方' },
+      { label: '账号ID', name: '22' }
+    ],
+    imgs: {
+      label: '资源图片',
+      list: [
+        'https://file.iviewui.com/iview-admin/login_bg.jpg',
+        'https://file.iviewui.com/iview-admin/login_bg.jpg',
+        'https://file.iviewui.com/iview-admin/login_bg.jpg'
+      ]
+    }
+  }
+
   async mounted() {
+    try {
+      const {
+        data: { user }
+      } = await infoIndex()
+    } catch (ex) {}
   }
 }
 </script>
 
 <style lang="less" scoped>
+@import '~@/site/lib.less';
+
+@c-bg: #fff;
+.compad {
+  padding: 20px 0 20px 30px;
+}
+.comli {
+  padding-bottom: 20px;
+  font-size: 14px;
+  color: @c-sub-text;
+}
+
+.content {
+  background: @c-bg;
+  h3 {
+    font-size: 14px;
+    height: 50px;
+    line-height: 50px;
+    padding-left: 30px;
+    background: @c-head-bg;
+  }
+  ul {
+    .compad;
+    li {
+      .comli;
+      div {
+        width: 80px;
+      }
+      p {
+        color: @c-text;
+        .imgs {
+          margin-right: 20px;
+        }
+      }
+    }
+  }
+  dl {
+    .compad;
+    height: 152px;
+    dd {
+      width: 50%;
+      float: left;
+      .comli;
+      div {
+        width: 80px;
+        float: left;
+      }
+      p {
+        color: @c-text;
+      }
+    }
+  }
+}
 </style>
---
+++ src/views/account/info/status.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="stateContent flex-box">
+    <div>
+      <img :src="status[statuCode].img" width="64px" alt="alias">
+    </div>
+    <div class="textMes">
+      <h6>{{status[statuCode].title}}</h6>
+      <p>{{status[statuCode].message}}</p>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Prop } from 'vue-property-decorator'
+import ViewBase from '@/util/ViewBase'
+
+@Component
+export default class InfoStatus extends ViewBase {
+  @Prop({ type: Number }) statuCode: any
+
+  status = [
+    {
+      img: require('../../../assets/icon/waiting.png'),
+      title: '未上传资质',
+      message: '您的账号尚未补全开户资质，请尽快上传营业执照通过账号审核！'
+    },
+    {
+      img: require('../../../assets/icon/waiting.png'),
+      title: '审核中',
+      message: '您的账号正在审核中，审核通过后即可开始上传资源位。'
+    },
+    {
+      img: require('../../../assets/icon/waiting.png'),
+      title: '审核失败',
+      message: '您的账号审核未通过，请修改注册信息。 '
+    },
+    {
+      img: require('../../../assets/icon/waiting.png'),
+      title: '正常使用',
+      message: '您的账号正常，可进行广告投放！如需变更公司账号请联系我司。！'
+    }
+  ]
+}
+</script>
+<style lang="less" scoped>
+@import '~@/site/lib.less';
+
+@c-bg: #fff;
+.stateContent {
+  padding: 50px 0 50px 60px;
+  background: @c-bg;
+  .textMes {
+    padding-left: 30px;
+    h6 {
+      font-size: 18px;
+      padding-bottom: 12px;
+      color: @c-head;
+    }
+    p {
+      font-size: 14px;
+      color: @c-sub-text;
+    }
+  }
+}
+</style>
+
