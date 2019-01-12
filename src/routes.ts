a3567b195e79388a74db5065a8fd6dcb979070f4
chen pengyu <webcpy@163.com>
2019-01-12 14:49:25 +0800

    图片 样式修改

--- src/routes.ts
+++ src/routes.ts
@@ -106,7 +106,7 @@ export default [
   },
   // 推广管理 - 广告计划 - 生成广告方案
   {
-    path: '/pop/plan/scheme',
+    path: '/pop/plan/scheme/:id',
     name: 'pop-plan-scheme',
     component: () => import('./views/pop/pages/index.vue'),
   },
