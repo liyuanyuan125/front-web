a417b096051d68ea270a5223af36c0ad6bfdd04e
zhangpeng <peng.zhang@aiads.com>
2019-01-14 16:42:37 +0800

    资源方菜单

---
+++ src/views/layout/allSiderMenuList.ts
@@ -0,0 +1,156 @@
+import { SystemCode } from '@/util/types'
+
+export interface SiderMenuItem {
+  name: string
+  label: string
+  systems?: SystemCode[]
+  subList?: SiderMenuItem[]
+}
+
+/**
+ * 全部左侧菜单列表，通过 systems 进行区分属于哪个「系统」
+ *
+ * 1、若某个菜单项没有 systems，则认为它属于全部系统
+ * 2、否则，按照 systems 进行区分
+ * 3、subList 中的某一项，也可能含有 systems 项
+ */
+const allSiderMenuList: SiderMenuItem[] = [
+  {
+    name: 'home',
+    label: '首页',
+  },
+
+  {
+    name: 'pop',
+    label: '推广管理',
+    systems: ['ads'],
+    subList: [
+      {
+        name: 'pop-plan',
+        label: '广告计划',
+      },
+      {
+        name: '',
+        label: '广告片',
+      },
+    ]
+  },
+
+  {
+    name: 'report',
+    label: '数据报表',
+    systems: ['ads'],
+    subList: [
+      {
+        name: 'report-plan',
+        label: '投放成效报告',
+      },
+    ]
+  },
+
+  {
+    name: 'customer',
+    label: '客户管理',
+    systems: ['ads'],
+    subList: [
+      {
+        name: 'report-plan',
+        label: '客户列表',
+      },
+    ]
+  },
+
+  {
+    name: 'customer',
+    label: '客户管理',
+    systems: ['ads'],
+    subList: [
+      {
+        name: 'report-plan',
+        label: '客户列表',
+      },
+    ]
+  },
+
+  {
+    name: 'order',
+    label: '广告单管理',
+    systems: ['resource'],
+    subList: [
+      {
+        name: 'report-plan',
+        label: '广告单列表',
+      },
+    ]
+  },
+
+  {
+    name: 'play',
+    label: '排播管理',
+    systems: ['resource'],
+    subList: [
+      {
+        name: 'report-plan',
+        label: '排播列表',
+      },
+    ]
+  },
+
+  {
+    name: 'space',
+    label: '资源位管理',
+    systems: ['resource'],
+    subList: [
+      {
+        name: 'report-plan',
+        label: '资源位列表',
+      },
+    ]
+  },
+
+  {
+    name: 'finance',
+    label: '财务管理',
+    systems: ['ads', 'resource'],
+    subList: [
+      {
+        name: 'finance-info',
+        label: '财务信息',
+      },
+      {
+        name: 'finance-bill',
+        label: '资金账单',
+      }
+    ]
+  },
+
+  {
+    name: 'account',
+    label: '账户管理',
+    systems: ['ads', 'resource'],
+    subList: [
+      {
+        name: 'account-info',
+        label: '账号信息',
+      },
+      {
+        name: 'account-user',
+        label: '用户管理',
+      },
+      {
+        name: 'account-auth',
+        label: '权限管理',
+      },
+      {
+        name: 'account-cinema',
+        label: '影院管理',
+      },
+      {
+        name: 'account-password',
+        label: '修改密码',
+      },
+    ]
+  },
+]
+
+export default allSiderMenuList
