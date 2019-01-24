cca0c64d249e92befbc1a58e504ee5616260d524
李园园 <yuanyuan.li@aiads.com>
2019-01-24 18:38:11 +0800

    激活邮箱样式修正

--- src/views/portal/activeEmail.vue
+++ src/views/portal/activeEmail.vue
@@ -1,25 +1,37 @@
 <template>
   <div class="activeEmail">
-    <Form :model="form" label-position="left" class="edit-input" :label-width="100">
+    <Form :model="form" ref="form" :rules="rules" label-position="left" class="edit-input" :label-width="100">
       <h3 class="logo">
-        <img src="../../assets/login/logo.png">
+        <img src="../../assets/login/logo.png" @click="toLogin">
       </h3>
-      <FormItem label="密码" class="item-top">
-        <Input v-model="form.firstPass"  placeholder="请设置包含大小写的英文字母与数字的组合，8-16 位" ></Input>
+      <FormItem label="密码" prop="firstPass" class="item-top">
+        <Input
+          v-model="form.firstPass"
+          type="password"
+          :maxlength="16"
+          placeholder="请输入包含大小写的英文字母与数字的组合，8-16 位"
+        ></Input>
       </FormItem>
-      <FormItem label="确定密码" class="item-top">
-        <Input v-model="form.secondPass" placeholder="请设置包含大小写的英文字母与数字的组合，8-16 位"></Input>
+      <FormItem label="确定密码" prop="secondPass">
+        <Input
+          v-model="form.secondPass"
+          type="password"
+          :maxlength="16"
+          placeholder="请输入包含大小写的英文字母与数字的组合，8-16 位"
+        ></Input>
       </FormItem>
     </Form>
     <div class="btnCenter">
-      <button class="button-ok addSumbit" @click="handleInforma">确定增加</button>
+      <button class="button-ok addSumbit" @click="handleInforma">激活邮箱</button>
     </div>
   </div>
 </template>
-<script>
+<script lang="ts">
 import { Component } from 'vue-property-decorator'
 import ViewBase from '@/util/ViewBase'
 import { activeEmailSumbit } from '@/api/user'
+import { confirm, toast } from '@/ui/modal'
+import { validatePassword } from '@/util/validateRules'
 
 @Component
 export default class Main extends ViewBase {
@@ -29,19 +41,54 @@ export default class Main extends ViewBase {
     firstPass: '',
     secondPass: ''
   }
+
+  get rules() {
+    return {
+      firstPass: [
+        {
+          required: true,
+          trigger: 'blur',
+          validator(rule: any, value: string, callback: any) {
+            const msg = validatePassword(value)
+            msg ? callback(new Error(msg)) : callback()
+          }
+        }
+      ],
+      secondPass: [
+        {
+          required: true,
+          trigger: 'blur',
+          validator: (rule: any, value: string, callback: any) => {
+            value != this.form.firstPass
+              ? callback(new Error('两次密码不一致，请重新输入'))
+              : callback()
+          }
+        }
+      ]
+    }
+  }
+
   mounted() {
     const url = this.$route.path
     const lastNum = url.lastIndexOf('/') + 1
     this.ticket = url.substr(lastNum)
   }
+  toLogin() {
+    this.$router.push({ name: 'login' })
+  }
   async handleInforma() {
+    const valid = await (this.$refs.form as any).validate()
+    if (!valid) {
+      return
+    }
     try {
       await activeEmailSumbit({
         password: this.form.firstPass,
         ticket: this.ticket
       })
+      toast('已激活成功')
     } catch (ex) {
-      this.handleError(ex)
+      this.handleError(ex.msg)
     }
   }
 }
@@ -51,7 +98,7 @@ export default class Main extends ViewBase {
   position: absolute;
   left: 50%;
   top: 50%;
-  transform: translate(-50%, -80%);
+  transform: translate(-50%, -55%);
   .logo {
     text-align: center;
     padding-bottom: 60px;
