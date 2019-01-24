ecc046739a42c577ab19cb751d2958f63edf8e80
李园园 <yuanyuan.li@aiads.com>
2019-01-24 17:19:54 +0800

    激活邮箱name变更

--- src/main.ts
+++ src/main.ts
@@ -46,7 +46,7 @@ const router = new Router({
       }
     },
     {
-      path: '/accounts',
+      path: '/account-activate',
       name: 'active-email',
       component: activeEmail,
       meta: {
