7402c80ba307eae836dcdc8dde494edddc0b879a
李园园 <yuanyuan.li@aiads.com>
2019-01-24 13:31:49 +0800

    邮箱变更

--- src/views/layout/MainLayout.vue
+++ src/views/layout/MainLayout.vue
@@ -20,7 +20,7 @@
 
       <div class="flex-box">
         <span class="corp-name">{{systemName}}：{{user.companyName}}</span>
-        <span class="user-name">用户：{{user.name}}</span>
+        <span class="user-name">用户：{{user.email}}</span>
         <a class="logout" title="退出" @click="logout">退出</a>
       </div>
     </header>
