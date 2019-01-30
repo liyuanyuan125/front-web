91107fd12e6ae0e631b1c9ddb8b9d5f743ab36ae
chen pengyu <webcpy@163.com>
2019-01-30 15:35:42 +0800

    广告片详情修改

--- src/views/pop/pages/dlgdetail.vue
+++ src/views/pop/pages/dlgdetail.vue
@@ -5,45 +5,45 @@
   :width='750'
   @on-cancel="cancel()">
     <div style=" text-align: center">
-      <div class="mb50">
+      <div class="mb30">
         <span class="header-title">方案确认单</span>
       </div>
-      <div style='background:rgba(249, 249, 249, 1);padding: 10px 20px 0px 20px;'>
-        <Row class="mb20">
-          <Col :span="3">投放类型</Col>
+      <div class="mb20" style='background:rgba(249, 249, 249, 1);padding: 10px 20px 1px 20px;'>
+        <Row class="mb10">
+          <Col :span="4">投放类型</Col>
           <Col :span="7">
             <span>映前广告</span>
           </Col>
           <Col :span="4">广告计划名称</Col>
-          <Col :span="10"><span>{{data.name}}</span></Col>
+          <Col :span="9"><span>{{data.name}}</span></Col>
         </Row>
-        <Row class="mb20">
-          <Col :span="3">客户名称</Col>
+        <Row class="mb10">
+          <Col :span="4">客户名称</Col>
           <Col :span="7"><span>{{data.customerName}}</span></Col>
           <Col :span="4">广告片规格</Col>
-          <Col :span="10"><span>{{data.specification}}s</span></Col>
+          <Col :span="9"><span>{{data.specification}}s</span></Col>
         </Row>
-        <Row class="mb20">
-          <Col :span="3">关联广告片</Col>
+        <Row class="mb10">
+          <Col :span="4">关联广告片</Col>
           <Col :span="7"><span>{{data.advertisingName}}</span></Col>
           <Col :span="4">投放排期</Col>
-          <Col :span="10"><span>{{begin}}~{{end}}</span></Col>
+          <Col :span="9"><span>{{begin}}~{{end}}</span></Col>
         </Row>
-        <Row class="mb20">
-          <Col :span="3">档期</Col>
-          <Col :span="7"><span>春节档</span></Col>
+        <Row class="mb10">
+          <Col :span="4">档期</Col>
+          <Col :span="7"><span>{{data.calendarName}}</span></Col>
           <Col :span="4">投放天数</Col>
-          <Col :span="10"><span>{{day}}天</span></Col>
+          <Col :span="9"><span>{{day}}天</span></Col>
         </Row>
-        <Row class="mb20">
-          <Col :span="3">冻结金额/￥</Col>
-          <Col :span="7"><span class="money">789</span></Col>
+        <Row class="mb5">
+          <Col :span="4">预估冻结金额／¥</Col>
+          <Col :span="7"><span class="money">{{money}}</span></Col>
           <Col :span="4">计费类型</Col>
-          <Col :span="10"><span>CPM/30s</span></Col>
+          <Col :span="9"><span>CPM/30s</span></Col>
         </Row>
       </div>
       <div v-if='data.directionType == 2' style='background:rgba(249, 249, 249, 1);padding: 10px 20px 1px 20px; margin-top: 10px;'>
-        <Row class="mb20">
+        <Row class="mb10">
           <Col :span="3">投放影片</Col>
           <Col :span="5" class='im'>
             <!-- <div> -->
@@ -58,7 +58,7 @@
             <!-- </div> -->
           </Col>
         </Row>
-        <Row class="mb20">
+        <Row class="mb10">
           <Col :span="3">地域偏好</Col>
           <Col :span="21">
             河北省 | 北京市 | 上海市 | 安徽省 | 广东省
@@ -66,55 +66,55 @@
         </Row>
       </div>
       <div style='background:rgba(249, 249, 249, 1);padding: 10px 20px 0 20px; margin-top: 10px;'>
-        <Row class="mb20">
+        <Row class="mb10">
           <Col :span="4">投放区域 (3个)</Col>
           <Col :span="20">
             华北 | 华中 | 华南
           </Col>
         </Row>
-        <Row class="mb20">
+        <Row class="mb10">
           <Col :span="4">投放省份 (5个)</Col>
           <Col :span="20">
             河北省 | 北京市 | 上海市 | 安徽省 | 广东省
           </Col>
         </Row>
-        <Row class="mb20">
+        <Row class="mb10">
           <Col :span="4">投放城市 (6个)</Col>
           <Col :span="20">
-            <Row class="mb20">
+            <Row class="mb10">
               <Col :span="4">1级城市(2个)</Col>
               <Col :span="20">北京市 | 上海市</Col>
             </Row>
-            <Row class="mb20">
+            <Row class="mb10">
               <Col :span="4">2级城市(2个)</Col>
               <Col :span="20">广州市 | 佛山市</Col>
             </Row>
-            <Row class="mb20">
+            <Row class="mb10">
               <Col :span="4">3级城市(2个)</Col>
               <Col :span="20">石家庄市 | 合肥市</Col>
             </Row>
-            <Row class="mb20">
+            <Row class="mb10">
               <Col :span="4">4级城市(0个)</Col>
               <Col :span="20"></Col>
             </Row>
           </Col>
         </Row>
-        <Row class="mb20">
+        <Row class="mb10">
           <Col :span="4">投放影院 (60个)</Col>
           <Col :span="20">
-            <Row class="mb20">
+            <Row class="mb10">
               <Col :span="24">A级票房 (20个)</Col>
               <!-- <Col :span="20">北京市 | 上海市</Col> -->
             </Row>
-            <Row class="mb20">
+            <Row class="mb10">
               <Col :span="24">B级票房 (20个)</Col>
               <!-- <Col :span="20">广州市 | 佛山市</Col> -->
             </Row>
-            <Row class="mb20">
+            <Row class="mb10">
               <Col :span="24">C级票房 (20个)</Col>
               <!-- <Col :span="20">石家庄市 | 合肥市</Col> -->
             </Row>
-            <Row class="mb20">
+            <Row class="mb10">
               <Col :span="24">D级票房 (0个)</Col>
               <!-- <Col :span="20"></Col> -->
             </Row>
@@ -132,12 +132,14 @@
 <script lang="ts">
 import { Component } from 'vue-property-decorator'
 import ViewBase from '@/util/ViewBase'
+import { formatCurrency } from '@/fn/string'
 
 @Component
 export default class Main extends ViewBase {
   showDlg = false
   forMat: any = {}
   data: any = {}
+  money: any = ''
 
   get begin() {
     return this.formatTime(this.data.beginDate)
@@ -155,6 +157,7 @@ export default class Main extends ViewBase {
   init(forMat: any) {
     this.showDlg = true
     this.forMat = forMat
+    this.money = formatCurrency(this.forMat.estimateCostAmount * 1000)
   }
 
   created() {
@@ -193,7 +196,20 @@ export default class Main extends ViewBase {
   height: 50px;
   color: @text-color;
   background-color: @color;
+  margin-right: 20px;
 }
+.mb-cyc(8);
+.mb-cyc(@n, @i: 1) when (@i =< @n) {
+  @b: (@i * 5);
+  .mb@{b} {
+    margin-bottom: (@i * 5px);
+  }
+  .pd@{b} {
+    padding-top: (@i * 5px);
+  }
+  .mb-cyc(@n, (@i + 1));
+}
+
 .foot {
   text-align: center;
   height: 90px;
@@ -208,14 +224,21 @@ export default class Main extends ViewBase {
   font-weight: 500;
   font-style: normal;
   font-size: 20px;
-  color: #fe8135;
+  color: #000;
   padding: 5px 15px 8px 15px;
-  border-bottom: 2px solid #fe8135;
+  border-bottom: 2px solid #000;
 }
 /deep/ .ivu-modal-content {
   margin-top: -40px;
 }
+/deep/ .ivu-col-span-7 span:empty {
+  &::before {
+    content: '暂无';
+  }
+}
 .ivu-row .money {
+  display: inline-block;
+  margin-top: -8px;
   font-weight: bold;
   font-size: 20px;
   // color: #f90;
@@ -227,18 +250,6 @@ export default class Main extends ViewBase {
   span {
     color: #333;
   }
-}
-.pd5 {
-  padding-top: 5px;
-}
-.mb50 {
-  margin-bottom: 50px;
-}
-.mb10 {
-  margin-bottom: 10px;
-}
-.mb20 {
-  margin-bottom: 10px;
 }
 .im {
   height: 260px;
