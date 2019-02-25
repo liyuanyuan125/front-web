bbc810f680075e4987f11f578dbec7986e4b7aa3
李园园 <yuanyuan.li@aiads.com>
2019-02-25 17:18:49 +0800

    -修改tab效果

--- src/views/report/plan/summary.vue
+++ src/views/report/plan/summary.vue
@@ -1,3 +1,20 @@
 <template>
-    <div>汇总</div>
+    <div>
+        <p>汇总</p>
+        <p>{{value.beginDate}}</p>
+         <p>{{value.id}}</p>
+    </div>
 </template>
+<script lang="ts">
+import { Component, Prop } from 'vue-property-decorator'
+import ViewBase from '@/util/ViewBase'
+
+@Component
+export default class Main extends ViewBase {
+    @Prop({type: Object}) value: any
+
+    mounted() {
+        // console.log(this.value)
+    }
+}
+</script>
