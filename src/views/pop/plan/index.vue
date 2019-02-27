92467166e9309afb76772eb088ad16c1305db5ca
chen pengyu <webcpy@163.com>
2019-02-27 16:42:26 +0800

    地域偏好 不限修改

--- src/views/pop/plan/index.vue
+++ src/views/pop/plan/index.vue
@@ -726,7 +726,7 @@ export default class Main extends ViewBase {
   @Watch('cinema.MOVIE_TYPE', { deep: true })
   watchMOVIE_TYPE(value: any, oldValue: any) {
     // 不限与其他项互斥
-    if (value.length > 3) {
+    if (value.length > 3 && value[value.length - 1] != 0) {
       info('电影类型最多选3项')
       this.cinema.MOVIE_TYPE = value.slice(0, 3)
       this.typeName = this.tags[0].values.filter((it: any) => this.cinema.MOVIE_TYPE.includes(it.key))
@@ -757,7 +757,7 @@ export default class Main extends ViewBase {
   @Watch('form.tagTypeCode', { deep: true })
   watchformtagTypeCode(value: number[], oldValue: number[]) {
     // 不限与其他项互斥
-    if (value.length > 3) {
+    if (value.length > 3 && value[value.length - 1] != 0) {
       info('地域偏好最多选3项')
       this.form.tagTypeCode = value.slice(0, 3)
     } else {
