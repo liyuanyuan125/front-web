dbf15a3f144d1f4921b8d6923e7fe7eee56e2f1c
纪洁琼 <1910796669@qq.com>
2019-06-19 17:57:19 +0800

    第三方检测列表页

---
+++ src/api/testlist.ts
@@ -0,0 +1,45 @@
+
+// import { get } from '@/fn/ajax'
+import { get , post , put , del } from '@/fn/ajax'
+import { mockGet, tid, title20, typeInt, dateRange } from './mock'
+
+// 列表
+export async function queryList(query: any) {
+  const res = await get('/xadvert/v1/third-monitors' , query)
+  return res
+}
+
+
+// 新建合作申请
+export async function addlist(query: any) {
+  const res = await post(`/xadvert/v1/third-monitors`, query)
+  return res
+}
+
+// 详情
+export async function itemlist(id: any) {
+  const res = await get(`/xadvert/v1/third-monitors/${id}`)
+  return res
+}
+
+// 编辑合作申请
+export async function editlist(id: any , query: any) {
+  const res = await put(`/xadvert/v1/third-monitors/${id}`, query)
+  return res
+}
+
+// 上传表格
+export async function uplist( query: any) {
+  const res = await post(`/xadvert/v1/third-monitors/upload-cinemas`, query)
+  return res
+}
+
+
+// 删除
+export async function dels(id: any) {
+  const res = await del(`/xadvert/v1/third-monitors/${id}`)
+  return res
+}
+
+
+
