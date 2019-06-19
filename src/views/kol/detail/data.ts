2c7dd74236a81d1c11caf1d31c2b9726616105d4
zhangpeng <peng.zhang@aiads.com>
2019-06-19 16:39:23 +0800

    投放价格

--- src/views/kol/detail/data.ts
+++ src/views/kol/detail/data.ts
@@ -1,4 +1,5 @@
 import { get } from '@/fn/ajax'
+import { keyBy } from 'lodash'
 
 /**
  * KOL详情
@@ -21,6 +22,48 @@ export async function getKol({
     name,
     figure: photo,
   }
+
+  return result
+}
+
+/**
+ * 获取报价信息
+ * https://yapi.aiads-dev.com/project/144/interface/api/4238
+ * @param id kol id
+ */
+export async function getPrice(id: number) {
+  const {
+    data: {
+      priceList
+    }
+  } = await get(`/kol/accounts/${id}/delivery-prices`)
+
+  const priceMap = keyBy(priceList, 'channelCode')
+  const dealPrice = (type: string) => {
+    const list = ((priceMap[type] || {}).list || []).map(({ categoryName, settlementPrice }: any) => {
+      return {
+        name: categoryName,
+        value: `￥ ${settlementPrice}`,
+        enable: true,
+      }
+    })
+    return list
+  }
+
+  const channelList = [
+    { type: 'wechat', name: '微信' },
+    { type: 'weibo', name: '微博' },
+    { type: 'kuaishou', name: '快手' },
+    { type: 'douyin', name: '抖音' },
+  ]
+
+  const result = channelList.map(it => {
+    return {
+      ...it,
+      list: dealPrice(it.type)
+    }
+  })
+  .filter(it => it.list.length > 0)
 
   return result
 }
