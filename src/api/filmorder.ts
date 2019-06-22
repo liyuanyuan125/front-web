4ee25b4d3097f7c2813d6d82cee3a64223427e3b
聂晓东 <xiaodong.nie@aiads.com>
2019-06-22 18:00:49 +0800

    影片热度趋势

--- src/api/filmorder.ts
+++ src/api/filmorder.ts
@@ -1,11 +1,11 @@
 
 // import { get } from '@/fn/ajax'
-import { get , post , put } from '@/fn/ajax'
+import { get, post, put } from '@/fn/ajax'
 import { mockGet, tid, title20, typeInt, dateRange } from './mock'
 
 // 列表
 export async function queryList(query: any) {
-  const res = await get('/movie/cooperation/' , query)
+  const res = await get('/movie/cooperation/', query)
   return res
 }
 
@@ -28,14 +28,14 @@ export async function filmlist(id: any) {
 }
 
 // img
-export async function imgs(id: any , query: any) {
+export async function imgs(id: any, query: any) {
   const res = await put(`/movie/cooperation/${id}`, query)
   return res
 }
 
 // img
-export async function mains(id: any ) {
-  const res = await get(`/movie/${id}` )
+export async function mains(id: any) {
+  const res = await get(`/movie/${id}`)
   return res
 }
 
@@ -47,4 +47,178 @@ export async function mains(id: any ) {
 export async function trend(query: any, id: number | string = '') {
   const res = await get(`/movie/${id}/hot` , query)
   return res
+  return await mockGet(query, {
+    items: [
+      {
+        date: 20170101,
+        count: tid,
+        ranking: tid,
+        trend: tid,
+        channels: [
+          {
+            chanelCode: 'weibo',
+            count: tid,
+            ranking: tid,
+            trend: tid
+          },
+          {
+            chanelCode: 'toutiao',
+            count: tid,
+            ranking: tid,
+            trend: tid
+          },
+          {
+            chanelCode: 'wechat',
+            count: tid,
+            ranking: tid,
+            trend: tid
+          },
+          {
+            chanelCode: 'baidu',
+            count: tid,
+            ranking: tid,
+            trend: tid
+          }
+        ]
+      },
+      {
+        date: 20170102,
+        count: tid,
+        ranking: tid,
+        trend: tid,
+        channels: [
+          {
+            chanelCode: 'weibo',
+            count: tid,
+            ranking: tid,
+            trend: tid
+          },
+          {
+            chanelCode: 'toutiao',
+            count: tid,
+            ranking: tid,
+            trend: tid
+          },
+          {
+            chanelCode: 'wechat',
+            count: tid,
+            ranking: tid,
+            trend: tid
+          },
+          {
+            chanelCode: 'baidu',
+            count: tid,
+            ranking: tid,
+            trend: tid
+          }
+        ]
+      },
+      {
+        date: 20170103,
+        count: tid,
+        ranking: tid,
+        trend: tid,
+        channels: [
+          {
+            chanelCode: 'weibo',
+            count: tid,
+            ranking: tid,
+            trend: tid
+          },
+          {
+            chanelCode: 'toutiao',
+            count: tid,
+            ranking: tid,
+            trend: tid
+          },
+          {
+            chanelCode: 'wechat',
+            count: tid,
+            ranking: tid,
+            trend: tid
+          },
+          {
+            chanelCode: 'baidu',
+            count: tid,
+            ranking: tid,
+            trend: tid
+          }
+        ]
+      }
+    ],
+    channelCodeList: [
+      {
+        text: '米汇渠道',
+        key: 'mihui'
+      },
+      {
+        text: '头条',
+        key: 'toutiao'
+      },
+      {
+        text: '快手',
+        key: 'kuaishou'
+      },
+      {
+        text: '抖音',
+        key: 'douyin'
+      },
+      {
+        text: '百度',
+        key: 'baidu'
+      },
+      {
+        text: '微信',
+        key: 'wechat'
+      },
+      {
+        text: '微博',
+        key: 'weibo'
+      },
+      {
+        text: '艺恩',
+        key: 'entgoup'
+      },
+      {
+        text: '淘票票',
+        key: 'taopiaopiao'
+      },
+      {
+        text: '时光',
+        key: 'mtime'
+      },
+      {
+        text: '猫眼',
+        key: 'maoyan'
+      },
+      {
+        text: '豆瓣',
+        key: 'douban'
+      },
+      {
+        text: '小红书',
+        key: 'xiaohongshu'
+      },
+      {
+        text: '腾讯qq.com',
+        key: 'qq'
+      },
+      {
+        text: '爱奇艺',
+        key: 'iqiyi'
+      },
+      {
+        text: '优酷',
+        key: 'youku'
+      },
+      {
+        text: '搜狐',
+        key: 'sohu'
+      },
+      {
+        text: '官网',
+        key: 'official-website'
+      }
+    ]
+  })
 }
