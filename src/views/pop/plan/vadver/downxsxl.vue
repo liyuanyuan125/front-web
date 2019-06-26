4522f7aa5469ab45d53a3b5b4203d894fb278ba7
陈鹏宇 <1728252512@qq.com>
2019-06-26 20:55:51 +0800

    kol 列表修改 导出初步添加

---
+++ src/views/pop/plan/vadver/downxsxl.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <span @click="down">aaaaaaaaaa</span>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop } from 'vue-property-decorator'
+import ViewBase from '@/util/ViewBase'
+import { xlsl } from './excl'
+
+@Component
+export default class Dates extends ViewBase {
+  download: any = ''
+  href: any = ''
+
+  down() {
+    const tHeader = ['序号', '省份', '地区'] // 表头
+    const title = ['锚地船舶', '', '']  // 标题
+    const filterVal = ['NAME', 'VESSEL_LENGTH', 'RECOMMEND_BERTH']
+    const list = [{
+      NAME: '陈'
+    }]
+    const data = this.formatJson(filterVal, list)
+    // data.map(item => {
+    //         // console.log(item)
+    //   item.map((i, index) => {
+    //     if (!i) {
+    //       item[index] = ''
+    //     }
+    //   })
+    // })
+    const merges = ['A1:C1']
+     xlsl({
+      title,
+      header: tHeader,
+      data,
+      merges,
+      filename: '影片列表',
+      autoWidth: true,
+      bookType: 'xlsx'
+    })
+  }
+
+  formatJson(filterVal: any, jsonData: any) {
+    return jsonData.map((v: any) => filterVal.map((j: any) => v[j]))
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.setp-wrap {
+  height: 104px;
+  padding: 33px 30px 33px;
+  font-size: 14px;
+  font-weight: 500;
+  border-bottom: solid 10px #f2f2f2;
+  color: #2481d7;
+}
+</style>
--- src/views/pop/plan/vadver/exportfile.vue
+++ src/views/pop/plan/vadver/exportfile.vue
@@ -1,7 +1,19 @@
 <template>
   <div>
     <span @click="exportData">下载列表</span>
-  
+    <Table height="320" style="display: none" ref="table" stripe :columns="columns" :data="tableDate1">
+      <template v-if="tag == 1" slot-scope="{ row }" slot="citys">
+        {{row.provinceNname}} {{row.cityName}}
+      </template>
+
+      <template slot-scope="{ row }" slot="estimateShowCount">
+        {{formatNums(row.showCount)}}
+      </template>
+
+      <template slot-scope="{ row }" slot="estimatePersonCount">
+        {{formatNums(row.personCount)}}
+      </template>
+    </Table>
   </div>
 </template>
 
@@ -18,6 +30,61 @@ import { exportId } from '@/api/popPlan.ts'
 export default class Export extends ViewBase {
   @Prop() value!: any
 
+  tag = 1
+
+  get columns() {
+    const tag = ['影院名称', '影院名称', '城市名称', '省份名称']
+    const key = ['cinemaName', 'chainName', 'cityName', 'provinceName' ]
+    const one = [{
+      title: tag[this.tag - 1],
+      key: key[this.tag - 1],
+      align: 'center'
+    }]
+    const two = [
+      {
+        title: '包含城市',
+        slot: 'citys',
+        align: 'center'
+      }
+    ]
+    const therr = [
+      {
+        title: '包含影院',
+        key: 'containCinemas',
+        align: 'center'
+      }
+    ]
+    const four = [
+      {
+        title: '所在地',
+        slot: 'citys',
+        align: 'center'
+      }
+    ]
+    const five = [
+      {
+        title: '预估投放场次',
+        slot: 'estimateShowCount',
+        align: 'center'
+      },
+      {
+        title: '预估曝光人次',
+        slot: 'estimatePersonCount',
+        align: 'center'
+      }
+    ]
+    return [
+      ...one,
+      {
+        title: '所属院线',
+        key: 'chainName',
+        align: 'center'
+      },
+      ...four,
+      ...five
+    ]
+  }
+
   tableDate1: any = []
 
   formatNums(data: any) {
@@ -26,7 +93,8 @@ export default class Export extends ViewBase {
 
   async exportData() {
     try {
-      await exportId(this.value)
+      const { data } = await exportId(this.value)
+      this.tableDate1 = data.planCinemas || []
       this.down()
     } catch (ex) {
       this.handleError(ex)
@@ -34,7 +102,9 @@ export default class Export extends ViewBase {
   }
 
   down() {
-    // saveAs()
+   (this.$refs.table as any).exportCsv({
+      filename: '影院列表',
+    })
   }
 }
 </script>
