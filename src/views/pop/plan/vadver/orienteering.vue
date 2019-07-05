867f5836ab47ebfa0ec8d9dc8e91e736542dd1c2
zhangpeng <peng.zhang@aiads.com>
2019-07-05 14:13:06 +0800

    恢复这些文件

--- src/views/pop/plan/vadver/orienteering.vue
+++ src/views/pop/plan/vadver/orienteering.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="plan-box">
     <Spin v-if="spinshow" fix>
-      <img style="width: 200px" src="./assets/ad.gif"> 
+      <img style="width: 200px" src="./assets/ad.gif">
       <Progress :percent="spins" status="active" />
       <p style="font-size: 30px; color: #3959A8">{{spins}}%</p>
     </Spin>
@@ -78,7 +78,7 @@
                 <FormItem class="item-top form-item-type">
                   <RadioGroup v-model="form.sex" class="item-radio-top">
                     <Radio  style="width: 220px" class="check-item form-item-first" :label="0">不限</Radio>
-                    <Radio 
+                    <Radio
                       v-if="it.key != 'unknow'"
                       style="width: 220px; height: 70px;"
                       v-for="it in sexList"
