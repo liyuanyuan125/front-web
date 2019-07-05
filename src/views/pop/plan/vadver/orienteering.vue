7dbf75f9c1f068fdfd03d1e4541fee2e98d31b5c
陈鹏宇 <1728252512@qq.com>
2019-07-05 09:58:55 +0800

    动画修改

--- src/views/pop/plan/vadver/orienteering.vue
+++ src/views/pop/plan/vadver/orienteering.vue
@@ -514,8 +514,8 @@ export default class Orienteering extends ViewBase {
               } else {
                 info('非常抱歉，暂未找到匹配项；请尝试扩大定向范围或投放排期范围')
               }
+              this.spins = 0
             }, 500)
-            this.spins = 0
           // }
         } else {
           this.loddding()
