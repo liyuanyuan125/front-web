3a0d57a1d6aa3a4f387cf8bcc19461f698727b50
陈鹏宇 <1728252512@qq.com>
2019-07-10 17:19:33 +0800

    kol先禁止筛选

--- src/views/kol/kollist/index.vue
+++ src/views/kol/kollist/index.vue
@@ -7,22 +7,37 @@
         <div class="check-detail">
           <FormItem label="账号类别"  class="form-item-type">
             <RadioGroup  v-model="form.accountCategoryCode" class="item-radio-top">
-              <Radio :disabled="acount==2" class="check-item form-item-first" :label="0">不限</Radio>
-              <Radio v-if="it.controlStatus != 2" :disabled="acount==2" v-for="it in accountList" :key="it.key" :label="it.key"
-                class="check-item">{{it.text}}</Radio>
+              <Poptip trigger="hover"  content="content">
+                <Radio :disabled="acount==2" class="check-item form-item-first" :label="0">不限</Radio>
+                <Radio :disabled="true" v-if="it.controlStatus != 2"  v-for="it in accountList" :key="it.key" :label="it.key"
+                  class="check-item">{{it.text}}</Radio>
+                <div slot="content">
+                    <span>免费用户暂不支持筛选</span>
+                </div>
+              </Poptip>
             </RadioGroup>
           </FormItem>
           <FormItem label="粉丝数量"  class="form-item-type">
             <RadioGroup  v-model="form.fansRangCode" class="item-radio-top">
+              <Poptip trigger="hover"  content="content">
               <Radio :disabled="acount==2" class="check-item form-item-first" :label="0">不限</Radio>
-              <Radio :disabled="acount==2" v-for="it in fansList" :key="it.key" :label="it.key"
+              <Radio :disabled="true" v-for="it in fansList" :key="it.key" :label="it.key"
                 class="check-item">{{it.text}}</Radio>
+                <div slot="content">
+                    <span>免费用户暂不支持筛选</span>
+                </div>
+              </Poptip> 
             </RadioGroup>
           </FormItem>
           <FormItem label="地域分布"  class="form-item-type">
             <RadioGroup  v-model="area" class="item-radio-top">
               <Radio :disabled="acount==2" @click.native="areabox(false)" class="check-item form-item-first" :label="0">不限</Radio>
-              <Radio :disabled="acount==2" @click.native="areabox(true)" class="check-item" :label='1'>指定区域</Radio>
+              <Poptip trigger="hover"  content="content">
+              <Radio :disabled="true" @click.native="areabox(true)" class="check-item" :label='1'>指定区域</Radio>
+              <div slot="content">
+                    <span>免费用户暂不支持筛选</span>
+                </div>
+              </Poptip> 
             </RadioGroup>
           </FormItem>
           <div v-if="areaShow" class="area-box">
@@ -36,15 +51,25 @@
           <FormItem label="价格区间"  class="form-item-type">
             <RadioGroup  v-model="form.priceRangCode" class="item-radio-top">
               <Radio :disabled="acount==2" class="check-item form-item-first" :label="0">不限</Radio>
-              <Radio :disabled="acount==2" v-for="it in priceList" :key="it.key" :label="it.key"
+              <Poptip trigger="hover"  content="content">
+              <Radio :disabled="true" v-for="it in priceList" :key="it.key" :label="it.key"
                 class="check-item">{{it.text}}</Radio>
+                <div slot="content">
+                    <span>免费用户暂不支持筛选</span>
+                </div>
+              </Poptip> 
             </RadioGroup>
           </FormItem>
           <FormItem label="受众性别"  class="form-item-type">
             <RadioGroup  v-model="form.sex" class="item-radio-top">
               <Radio :disabled="acount==2" class="check-item form-item-first" :label="-1">不限</Radio>
-              <Radio :disabled="acount==2" v-for="it in sexList" :key="it.key" :label="it.key"
+              <Poptip trigger="hover"  content="content">
+              <Radio :disabled="true" v-for="it in sexList" :key="it.key" :label="it.key"
                 class="check-item">{{it.text}}</Radio>
+                <div slot="content">
+                    <span>免费用户暂不支持筛选</span>
+                </div>
+              </Poptip> 
             </RadioGroup>
           </FormItem>
         </div>
