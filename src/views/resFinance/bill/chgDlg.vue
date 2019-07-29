6c37c5ca0a2764a6b70ac5d0094ff10505ce1b2a
纪洁琼 <1910796669@qq.com>
2019-07-29 16:33:49 +0800

    对账单管理列表

--- src/views/resFinance/bill/chgDlg.vue
+++ src/views/resFinance/bill/chgDlg.vue
@@ -6,9 +6,9 @@
     :title="'审核账单'"
     @on-cancel="cancel" >
     <Form ref="dataForm" :model="dataForm" label-position="left"  :label-width="100">
-      <FormItem label="审核意见" prop="status">
-        <RadioGroup v-model="dataForm.status" >
-          <Radio v-for="it in statuslist" :key="it.key" :value="it.key" :label="it.key">{{it.text}}</Radio>
+      <FormItem label="审核意见" prop="agree">
+        <RadioGroup v-model="dataForm.agree" >
+          <Radio v-for="it in statuslist" :key="it.key" :value="it.value" :label="it.key">{{it.text}}</Radio>
         </RadioGroup>
       </FormItem>
       <FormItem label="备注" prop="remark">
@@ -31,9 +31,8 @@ import { queryList , approval } from '@/api/bill'
 import ViewBase from '@/util/ViewBase'
 const timeFormat = 'YYYY-MM-DD'
 const dataForm = {
-  orderItemId: 0,
-  confirmPrice: '',
-  confirmRemark: '',
+  agree: '',
+  remark: '',
 }
 
 @Component
@@ -41,52 +40,55 @@ export default class ComponentMain extends ViewBase {
 
   // loading = false
   showDlg = false
-  id = 0
+  id: any = 0
+  ids: any = []
 
   statuslist: any = [
     {
       key: 1,
-      text: '审核通过'
+      text: '审核通过',
+      value: '是'
     },
     {
       key: 2,
-      text: '审核拒绝'
+      text: '审核拒绝',
+      value: '否'
     }
   ]
 
   dataForm: any = { ...dataForm }
 
-  // init(id: number , price: any , mark: any) {
-  //   this.showDlg = true
-  //   this.dataForm.orderItemId = id
-  //   this.dataForm.confirmPrice = price
-  //   this.dataForm.confirmRemark = mark
-  //   this.id = id || 0
-  //   ; (this.$refs.dataForm as any).resetFields()
-  //   if (this.id) {
-  //       // console.log(this.id)
-  //   }
-  // }
+  init(id: any , ids: any) {
+    this.showDlg = true
+    this.id = id || 0
+    ; (this.$refs.dataForm as any).resetFields()
+    if (this.id == 0) {
+        // console.log(this.id)
+      this.ids = [id]
+    }
+    if (this.id != 0) {
+      this.ids = ids
+    }
+  }
 
-  // cancel() {
-  //   // this.dataForm.beginDate
-  //   this.showDlg = false
-  //   ; (this.$refs.dataForm as any).resetFields()
-  // }
+  cancel() {
+    this.showDlg = false
+    ; (this.$refs.dataForm as any).resetFields()
+  }
 
-  // // 表单提交
-  // async dataFormSubmit() {
-  //   try {
-  //     const res =  await edit (this.dataForm)
-  //     toast('操作成功')
-  //     this.showDlg = false
-  //     this.$emit('done' )
-  //     ; (this.$refs.dataForm as any).resetFields()
-  //   } catch (ex) {
-  //     this.handleError(ex)
-  //     this.showDlg = false
-  //   }
-  // }
+  // 表单提交
+  async dataFormSubmit() {
+    try {
+      const res =  await approval ({...this.dataForm , ids: this.ids})
+      toast('操作成功')
+      this.showDlg = false
+      this.$emit('done' )
+      ; (this.$refs.dataForm as any).resetFields()
+    } catch (ex) {
+      this.handleError(ex)
+      this.showDlg = false
+    }
+  }
 
   // async mounted() {
   //   try {
