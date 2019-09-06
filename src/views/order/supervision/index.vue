565ca51d11daf5cbea53ed14581ab3839f176f01
纪洁琼 <1910796669@qq.com>
2019-09-06 14:39:30 +0800

    监播示例按钮样式调整

--- src/views/order/supervision/index.vue
+++ src/views/order/supervision/index.vue
@@ -487,9 +487,9 @@ export default class Main extends ViewBase {
       border-radius: 50%;
       cursor: pointer;
       img {
-        width: 80%;
-        height: 80%;
-        margin: 10% 0 0 10%;
+        width: 60%;
+        height: 60%;
+        margin: 20% 0 0 20%;
       }
     }
     em {
