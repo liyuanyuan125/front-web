a227f849316447d17d8edf09a83732685cda6a79
yuanyuan.li <yuanyuan.li@jydata.com>
2019-10-24 11:07:53 +0800

    用户添加elevator

--- src/views/account/user/resEditDlg.vue
+++ src/views/account/user/resEditDlg.vue
@@ -42,7 +42,9 @@
           :current="current"
           :page-size="pageSize"
           show-total
+          show-sizer
           show-elevator
+          :page-size-opts = "[10, 20, 50, 100, 200]"
           @on-change="handlepageChange"
           @on-page-size-change="handlePageSize"
         />
