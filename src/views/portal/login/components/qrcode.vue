08229a5e7060ad7835863380992d0c5b018e5ae6
yuanyuan.li <yuanyuan.li@jydata.com>
2019-11-21 14:46:12 +0800

    -m

---
+++ src/views/portal/login/components/qrcode.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <img src="../assets/QR.png" class="qr-code" width="150" />
+  </div>
+</template>
+
+<script lang='ts'>
+import { Component } from 'vue-property-decorator'
+import ViewBase from '@/util/ViewBase'
+
+@Component
+export default class Main extends ViewBase {
+}
+
+</script>
+
+<style lang='less' scoped>
+.qr-code {
+  position: fixed;
+  bottom: 20px;
+  right: 30px;
+}
+</style>
